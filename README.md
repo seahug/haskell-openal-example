a98503f43a35f47eb7162f10f4219cb29bb01456
Richard Cook <rcook@rcook.org>
2018-08-18 14:31:37 -0700

    Copy example

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # play-sound
+
+## Prerequisites
+
+### Ubuntu
+
+```bash
+sudo apt-get install freeglut3-dev libalut-dev libopenal-dev
+```
+
+### macOS
+
+```bash
+brew install alut
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,22 @@
-module Main where
+-- From https://bluishcoder.co.nz/articles/haskell/openal.html
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import Control.Monad
+import Sound.ALUT
+
+playSound :: IO ()
+playSound =
+  withProgNameAndArgs runALUTUsingCurrentContext $ \_ _ ->
+  do
+    (Just device) <- openDevice Nothing
+    (Just context) <- createContext device []
+    currentContext $= Just context
+    buffer1 <- createBuffer $ Sine 440 0 1
+    buffer2 <- createBuffer HelloWorld
+    [source] <- genObjectNames 1
+    queueBuffers source [buffer1,buffer2]
+    play [source]
+    sleep 4
+    closeDevice device
+    return ()
+
+main = playSound
